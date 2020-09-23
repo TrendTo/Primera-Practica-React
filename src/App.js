71688c7420f263e20ca5c26de74dff40eb9097bb
Jonathan Potosi <jonathan.wladimir@gmail.com>
2020-09-23 18:37:57 -0500

    props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function Hello(){
+function Hello(props){
   return(
-    <div id="second">Hello World</div>
+  <div id="second">
+    <h5>{props.sub}</h5>
+    Hello {props.tx}
+  </div>
   )
 }
 
 function App() {
   return (
     <div className="App">
-      Bienvenido a este nuevo curso: <Hello/><Hello/>
+      Bienvenido a este nuevo curso: <Hello tx="Juan" sub="Usuario"/><Hello tx="Maria" sub="Cliente"/><Hello tx="El niño" sub="Admin"/>
     </div>
   );
 }
