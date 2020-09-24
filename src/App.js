7bb5b6c6a2ca4aa3864b073ee5331d86edeba421
Jonathan Potosi <jonathan.wladimir@gmail.com>
2020-09-23 19:12:42 -0500

    setState in the class

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import React from 'react';
 import './App.css';
 
-function Hello(props){
-  return(
-  <div id="second">
-    <h5>{props.sub}</h5>
-    Hello {props.tx}
-  </div>
-  )
+// function Hello(props){
+//   return(
+//   <div id="second">
+//     <h5>{props.sub}</h5>
+//     Hello {props.tx}
+//   </div>
+//   )
+// }
+
+class Hello extends React.Component{
+  
+  state = {
+    show: 'true'
+  }
+
+  toggle = () => {
+    this.setState({
+      show : !this.state.show
+    })
+  }
+
+  render(){
+    if (this.state.show) {
+      return(
+        <div>
+          <h5>{this.props.sub}</h5>
+          Hello {this.props.tx}
+          <button onClick={this.toggle}>
+            Click
+          </button>  
+        </div>
+        )
+    } else {
+      return(
+      <div>
+        There are not elements
+        <button onClick={this.toggle}>
+          Click
+        </button>  
+      </div>
+      )  
+    }
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      Bienvenido a este nuevo curso: <Hello tx="Juan" sub="Usuario"/><Hello tx="Maria" sub="Cliente"/><Hello tx="El niño" sub="Admin"/>
+      Bienvenido a este nuevo curso:
+      <Hello tx="Juan" sub="Usuario"/>
+      <Hello tx="Maria" sub="Cliente"/>
+      <Hello tx="El niño" sub="Admin"/>
     </div>
   );
 }
