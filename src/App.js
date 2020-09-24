640c727829fc5b42f8d1314942429f552aeb0dc6
Jonathan Potosi <jonathan.wladimir@gmail.com>
2020-09-24 16:35:55 -0500

    Task OneForm

--- src/App.js
+++ src/App.js
@@ -1,60 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-// function Hello(props){
-//   return(
-//   <div id="second">
-//     <h5>{props.sub}</h5>
-//     Hello {props.tx}
-//   </div>
-//   )
-// }
+import tasks from './sample/tasks.json'
 
-class Hello extends React.Component{
+class NewApp extends Component{
   
   state = {
-    show: 'true'
+    tasks : tasks
   }
-
-  toggle = () => {
-    this.setState({
-      show : !this.state.show
-    })
-  }
-
+  
   render(){
-    if (this.state.show) {
-      return(
-        <div>
-          <h5>{this.props.sub}</h5>
-          Hello {this.props.tx}
-          <button onClick={this.toggle}>
-            Click
-          </button>  
-        </div>
-        )
-    } else {
-      return(
-      <div>
-        There are not elements
-        <button onClick={this.toggle}>
-          Click
-        </button>  
-      </div>
-      )  
-    }
+    return <div>
+      {this.state.tasks.map(e => <p key={e.id}>
+        {e.title} - {e.descripcion} - {e.done}
+      </p>)}
+    </div>
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      Bienvenido a este nuevo curso:
-      <Hello tx="Juan" sub="Usuario"/>
-      <Hello tx="Maria" sub="Cliente"/>
-      <Hello tx="El niño" sub="Admin"/>
-    </div>
-  );
-}
-
-export default App;
+export default NewApp;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import NewApp from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NewApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
