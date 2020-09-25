206dd59fbf2eab81ddf9a0e518c1fbe6374c1b7e
Jonathan Potosi <jonathan.wladimir@gmail.com>
2020-09-24 20:50:50 -0500

    Function upd to handle css through js

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 
 class Task extends Component{
 
+    upd(){
+        return {
+            background : this.props.task.done ? 'green' : 'none',
+            color : this.props.task.done ? 'gray' : 'black'
+        }
+    }
+
     render(){
 
         const {task} = this.props
@@ -10,7 +17,7 @@ class Task extends Component{
             {task.title} -
             {task.descripcion} -
             {task.done}
-            <button>Edit</button>
+            <button style={this.upd()}>Edit</button>
             <button style={rm}>Remove</button>
         </div>
     }
